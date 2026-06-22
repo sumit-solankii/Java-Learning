832207d6695f77f17de272c91f603a5f6bde6efe
sumit-solankii <sumitkumarsolanki124@gmail.com>
2026-06-22 13:18:42 +0530

    Variables practising

--- variables.java
+++ variables.java
@@ -3,6 +3,13 @@ public class variables {
     public static void main(String[] args) {
         //variables-->
         int i = 5;
-        System.out.println("i is " + i);
+        System.out.println("i is " + i); // i is 5
+
+        byte b = 10;
+        System.out.println("b is " + b); // b is 10
+
+        b = (byte)i;
+        System.out.println("b is now " + 5); // b is now 5
+
     }
 }

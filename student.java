d7c137ef7501b4833ae5221b618cdd3ed04b704c
sumit-solankii <sumitkumarsolanki124@gmail.com>
2026-07-22 07:43:55 +0530

    print data

--- student.java
+++ student.java
@@ -1,8 +1,20 @@
 class student {
     private String name, city;
     private int age, marks;
-    void getData(String name, ) {
+    void getData(String n, String c, int a, int m) {
+        name = n;
+        city = c;
+        age = a;
+        marks = m;
+    }
 
+    void printData() {
+        System.out.println("Enter your name : " + name);
+        System.out.println("Enter your city : " + city);
+        System.out.println("Enter your age : " + age);
+        System.out.println("Enter your marks : " + marks);
     }
     
 }
+
+
